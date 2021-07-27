7cb82db923996736975b1c0f10e82b78e6e6b074
RyanTwT354 <84882715+RyanTwT354@users.noreply.github.com>
2021-07-26 17:54:33 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload() {
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup() {
+canvas = createCanvas(500, 500);
+canvas.center();
+}
+
+function draw() {
+image(img, 0,0,500,500)
+}
